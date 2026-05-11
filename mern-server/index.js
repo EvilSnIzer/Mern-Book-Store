cc7c86997248b3746cc9c51fddc35cace22c6e49
Manan Sharma <98555334+EvilSnIzer@users.noreply.github.com>
2026-05-12 00:59:29 +0530

    Secure: Move MongoDB URI to environment variables

--- mern-server/index.js
+++ mern-server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+require("dotenv").config();
 
 //middleware
 app.use(cors());
@@ -11,13 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//uVOwDtZdcuFHCVPk
-
 //mongodb configration
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://mern-book-store:uVOwDtZdcuFHCVPk@cluster0.2fopyq2.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
